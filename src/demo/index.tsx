a6a8999e7223052fb04ce07d3bcc3437ff3d4a27
hebiao.tan <hebiao.tan@77hub.com>
2020-12-19 17:38:38 +0800

    添加配置

---
+++ src/demo/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component{
+  render(){
+    return <p>123</p>
+  }
+}
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,45 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/demo/index')
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/demo/index'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          'babel-loader',
+          'ts-loader'
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/demo/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  devServer: {
+    port: 3000,
+    progress: true,
+    contentBase: path.resolve(__dirname, './src/demo')
+  }
 }
