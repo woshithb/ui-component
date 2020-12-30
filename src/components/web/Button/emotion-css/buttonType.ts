f6d39c3c10fbb8386c90ee72125289fa3b6dff99
hebiao.tan <hebiao.tan@77hub.com>
2020-12-30 22:28:39 +0800

    buttontype css基本完成

--- src/components/web/Button/emotion-css/buttonType.ts
+++ src/components/web/Button/emotion-css/buttonType.ts
@@ -1,4 +1,16 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+
+const btn_focus_and_blur = keyframes`
+	0% {
+		box-shadow: 0px 0px 0px 0px #69c0ff;
+	}
+	50% {
+		box-shadow: 0px 0px 6px 0px #69c0ff;
+	}
+	100% {
+		box-shadow: 0px 0px 0px 0px #69c0ff;
+	}
+`;
 
 export const button_primary = css`
 	border: 1px solid #d9d9d9;
@@ -9,7 +21,7 @@ export const button_primary = css`
 		background-color: #40a9ff;
 	}
 	&:focus {
-		animation-name: ;
+		animation-name: ${btn_focus_and_blur};
 		animation-duration: 1s;
 		border-color: #40a9ff;
 		background-color: #40a9ff;
@@ -21,6 +33,94 @@ export const button_primary = css`
 		color: rgba(0, 0, 0, 0.25);
 		background: #f5f5f5;
 		border-color: #d9d9d9;
+		cursor: not-allowed;
+	}
+`;
+
+export const button_default = css`
+	border: 1px solid #d9d9d9;
+	background-image: none;
+	background-color: #ffffff;
+	color: rgba(0, 0, 0, 0.85);
+	&:hover {
+		color: #40a9ff;
+		border-color: #40a9ff;
+	}
+	&:focus {
+		animation-name: ${btn_focus_and_blur};
+		animation-duration: 1s;
+		border-color: #40a9ff;
+		color: #40a9ff;
+	}
+	&.active {
+		background-color: #1890ff;
+	}
+	&.disabled {
+		color: rgba(0, 0, 0, 0.25);
+		background: #f5f5f5;
+		border-color: #d9d9d9;
+		cursor: not-allowed;
+	}
+`;
+
+export const button_dashed = css`
+	border: 1px dashed #d9d9d9;
+	background-image: none;
+	background-color: #ffffff;
+	color: rgba(0, 0, 0, 0.85);
+	&:hover {
+		color: #40a9ff;
+		border-color: #40a9ff;
+	}
+	&:focus {
+		animation-name: ${btn_focus_and_blur};
+		animation-duration: 1s;
+		border-color: #40a9ff;
+		color: #40a9ff;
+	}
+	&.active {
+		background-color: #1890ff;
+	}
+	&.disabled {
+		color: rgba(0, 0, 0, 0.25);
+		background: #f5f5f5;
+		border-color: #d9d9d9;
+		cursor: not-allowed;
+	}
+`;
+
+export const button_text = css`
+	border: none;
+	background-image: none;
+	background-color: #ffffff;
+	color: rgba(0, 0, 0, 0.85);
+	&:hover {
+		background-color: #f5f5f5;
+	}
+	&.active {
+		background-color: #f5f5f5;
+	}
+	&.disabled {
+		color: #f5f5f5;
+		background: #ffffff;
+		cursor: not-allowed;
+	}
+`;
+
+export const button_link = css`
+	border: none;
+	background-image: none;
+	background-color: #ffffff;
+	color: #1890ff;
+	&:hover {
+		color: #40a9ff
+	}
+	&.active {
+		color: #40a9ff;
+	}
+	&.disabled {
+		color: #f5f5f5;
+		background: #ffffff;
 		cursor: not-allowed;
 	}
 `;
